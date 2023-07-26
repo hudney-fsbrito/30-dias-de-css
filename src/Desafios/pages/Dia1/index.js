993ef6e2371d0343c6ff376860f44a020edf0284
hudney-fsbrito <hudney.rse@gmail.com>
2023-07-26 10:40:23 -0300

    Ajuste na página home

--- src/Desafios/pages/Dia1/index.js
+++ src/Desafios/pages/Dia1/index.js
@@ -2,35 +2,36 @@ import styleDia1 from "./dia1.module.css"
 
 function IconeSocial(props) {
   return (
-    <div className={styleDia1}>
-      <ul>
+    <div className={styleDia1.div}>
+      <ul className={styleDia1.ul}>
+        <li className={styleDia1.li}>
+          <span className={styleDia1.span}></span>
+          <span className={styleDia1.span}></span>
+          <span className={styleDia1.span}></span>
+          <span className={styleDia1.span}></span>          
+          <img className={styleDia1.image} width="100" height="100" src="https://img.icons8.com/ios-filled/100/228BE6/github.png" alt="github" />
+        </li>
+        
         <li>
           <span></span>
           <span></span>
           <span></span>
           <span></span>
-          <img width="100" height="100" src="https://img.icons8.com/ios-filled/100/228BE6/github.png" alt="github" />
+          <img className={styleDia1.image} width="100" height="100" src="https://img.icons8.com/ios-filled/100/228BE6/facebook-circled--v1.png" alt="facebook-circled--v1" />
         </li>
         <li>
           <span></span>
           <span></span>
           <span></span>
           <span></span>
-          <img width="100" height="100" src="https://img.icons8.com/ios-filled/100/228BE6/facebook-circled--v1.png" alt="facebook-circled--v1" />
-        </li>
-        <li>
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-          <img width="100" height="100" src="https://img.icons8.com/ios-filled/100/228BE6/twitter-circled--v1.png" alt="twitter-circled--v1" />
+          <img className={styleDia1.image} width="100" height="100" src="https://img.icons8.com/ios-filled/100/228BE6/twitter-circled--v1.png" alt="twitter-circled--v1" />
           </li>
         <li>
           <span></span>
           <span></span>
           <span></span>
           <span></span>
-          <img width="100" height="100" src="https://img.icons8.com/ios-filled/100/228BE6/linkedin-circled--v1.png" alt="linkedin-circled--v1" />
+          <img className={styleDia1.image} width="100" height="100" src="https://img.icons8.com/ios-filled/100/228BE6/linkedin-circled--v1.png" alt="linkedin-circled--v1" />
           </li>
       </ul>
     </div>
--- src/Desafios/pages/Home/index.js
+++ src/Desafios/pages/Home/index.js
@@ -1,15 +1,25 @@
 import { Link } from "react-router-dom";
-// import IconeSocial from "../Dia1";
-// import IconeSocial from "../Dia1";
+import styleHome from './Home.module.css'
+import imageHomeDia1 from "../../assets/Dia1.gif"
+import imageHomeDia2 from "../../assets/Dia2.gif"
 
 function Home() {
+  const images = [imageHomeDia1, imageHomeDia2]
 
   return (
 
     <header className="App-header">
       <h1>30 dias de CSS</h1>
-      <Link to='/dia1'><button>Dia 1</button></Link>
-      <Link to='/dia2'><button>Dia 2</button></Link>
+
+      <section className={styleHome.section}>
+        {images.map((image, i) => (
+          <Link to={'/dia' + (i + 1)} target="_blank"><img className={styleHome.image} src={image} alt="Gif dos projetos do desafio"></img></Link>
+
+        ))}
+      </section>
+
+      {/* <Link to='/dia1' target="_blank"><img className={styleHome.image} src={imageDia1} alt="Animação de ícone em camadas"></img><button>Dia 1</button></Link>
+      <Link to='/dia2' target="_blank"><img className={styleHome.image} src={imageDia2} alt="Animação de ícone em camadas"></img><button>Dia 2</button></Link> */}
     </header >
 
   );
